c4fbce975c9935c89a799a81dfabc15d4ced0413
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2014-02-07 18:02:54 -0700

    / ‘curling-10/brute-force.sh’

--- curling-10/brute-force.sh
+++ curling-10/brute-force.sh
@@ -2,6 +2,18 @@
 
 for i in {0..255}; do
 
+    HEX=$(printf '%x\n' $i)
+
+    URL="http://[2a02:6b8:0:141f:fea9:d5ff:fed5:${HEX}01]"
+
+    RESPONSE=$(curl -sL -o /dev/null -w "%{http_code}" -g <url>)
+
+    if [[ RESPONSE != 200 ]]; then
+
+        echo
+
+    fi
+
     OUT=$(curl -g "http://[2a02:6b8:0:141f:fea9:d5ff:fed5:${i}01]")
 
     echo "${i} :: ${OUT}"
