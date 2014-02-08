010d663a15622bbbdca862c16b89907c183e799c
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2014-02-07 17:55:24 -0700

    / ‘curling-10/brute-force.sh’

--- curling-10/brute-force.sh
+++ curling-10/brute-force.sh
@@ -2,7 +2,7 @@
 
 for i in $(seq -f "%02g" 0 99); do
 
-    OUT=$(curl -g "http://[2a02:6b8:0:141f:fea9:d5ff:fed5:${i}01]"
+    OUT=$(curl -g "http://[2a02:6b8:0:141f:fea9:d5ff:fed5:${i}01]")
 
     echo "${i} :: ${OUT}"
 
